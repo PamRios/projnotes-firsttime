fe2edec1b41e726cd56fa2a4656fa1965b5270a2
PamRios <124759381+PamRios@users.noreply.github.com>
2023-02-22 14:51:12 +0000

    😎 Creación de vista author

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { render } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,6 +8,19 @@ router.get('/', function(req, res, next) {
   let icon = iconSet[Math.floor(Math.random()*3)];
   res.render('index', { title: 'DWPII-2023A', icon}); //render permite renderizar una plantilla los datos se especifican en formato json
 }); //invoca una vista -> se genera por un template engiene handelbars (hbs) 
+
+router.get('/author', (req, res)=>{
+  let author= {
+    "Name": "Pam",
+    "Lastname": "Rios",
+    "COD": "00Thunder76",
+    "Occupation": "Student"
+  };
+
+  //Sending the view-model to be rendered by 
+  res.render('author', author);
+
+});
 
 module.exports = router;
 
