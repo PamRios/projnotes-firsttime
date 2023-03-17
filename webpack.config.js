5de7831e92e52ea24726cb1aad9247edb34fd31b
PamRios <124759381+PamRios@users.noreply.github.com>
2023-03-17 04:01:51 +0000

    😔 Does not work

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js"
-  }, 
-  devServer: {
-    static: path.join(__dirname, "public"),
-    port: 8080,
-    host: "localhost"
   }
 }
---
+++ webpack.dev.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports =  {
+    entry: "./client/index.js",
+    output:{
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    },
+
+    devServer: {
+        static: path.join(__dirname, "public"),
+        port: 8080,
+        host: "localhost"
+    }
+}
