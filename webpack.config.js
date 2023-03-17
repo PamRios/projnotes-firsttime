ef4a0f2ba1c3cecafb83f81c29ab8afc7bbbc1d8
PamRios <124759381+PamRios@users.noreply.github.com>
2023-03-17 03:52:47 +0000

    new Branch

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js"
-  },
+  }, 
   devServer: {
     static: path.join(__dirname, "public"),
     port: 8080,
