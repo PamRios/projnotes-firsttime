18c1418299d02b3ed5a1e26c834208930600e13b
PamRios <124759381+PamRios@users.noreply.github.com>
2023-04-23 18:27:08 +0000

    🏵 Extra correción de estilos AirBnB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,25 +1,26 @@
-//let express = require('express'); //importing express library
+// let express = require('express'); //importing express library
 import express from 'express';
-//creating a router instance
-//let router = express.Router();
-const {Router} = express;
+// creating a router instance
+// let router = express.Router();
+const { Router } = express;
 const router = Router();
 
-//creating the route 
-router.get('/author', (req, res)=>/*Callback */{ /*se ejecuta hasta que alguien haga una petición a /author */
-res.json({
-    "name": "Pam",
-    "Lastname": "Rios G",
-    "COD mobile": "00Thunder76",
-    "Occupation": "Student"   
-});
+// creating the route
+router.get('/author', (req, res) => {
+  /* se ejecuta hasta que alguien haga una petición a /author */
+  res.json({
+    ' name': 'Pam',
+    ' Lastname': 'Rios G',
+    'COD mobile': '00Thunder76',
+    ' Occupation': 'Student',
+  });
 });
 
-//Exporting the router
-//module.exports = router;
+// Exporting the router
+// module.exports = router;
 export default router;
 /*
 function(obj1, obj2, obj3 maybe
 function(petición, respuesta)
 function(req, res)
-(req, res)=>{ == function(req, res) {*/
+(req, res)=>{ == function(req, res) { */
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,30 @@
 import express from 'express';
-const {Router} = express;
-/*const render  = require('../app');
+
+const { Router } = express;
+/* const render  = require('../app');
 let router = express.Router(); */
 const router = Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  let iconSet=["✨", "🎃", "🎨"];
-  let icon = iconSet[Math.floor(Math.random()*3)];
-  res.render('index', { title: 'DWPII-2023A', icon}); //render permite renderizar una plantilla los datos se especifican en formato json
-}); //invoca una vista -> se genera por un template engiene handelbars (hbs) 
+router.get('/', (req, res) => {
+  const iconSet = ['✨', '🎃', '🎨'];
+  const icon = iconSet[Math.floor(Math.random() * 3)];
+  res.render('index', { title: 'DWPII-2023A', icon }); // render permite renderizar una plantilla los datos se especifican en formato json
+}); // invoca una vista -> se genera por un template engiene handelbars (hbs)
 
-router.get('/author', (req, res)=>{
-  let author= {
-    "Name": "Pam",
-    "Lastname": "Rios",
-    "COD": "00Thunder76",
-    "Occupation": "Student"
+router.get('/author', (req, res) => {
+  const author = {
+    ' name': 'Pam',
+    ' Lastname': 'Rios G',
+    'COD mobile': '00Thunder76',
+    ' Occupation': 'Student',
   };
 
-  //Sending the view-model to be rendered by 
+  // Sending the view-model to be rendered by
   res.render('author', author);
-
 });
 
-//module.exports = router;
+// module.exports = router;
 export default router;
 
 // ('index', { title: 'Express' }) view model html puro
